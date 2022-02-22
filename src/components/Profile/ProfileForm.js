923132f1a6b2269ce6603628b302758cd0f16b60
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-21 22:24:49 -0700

    added validation for passwords and logic to catch error

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -3,6 +3,10 @@ import { useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
 import classes from "./ProfileForm.module.css";
 
+const checkPassword = (password) => {
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/.test(password);
+};
+
 const ProfileForm = () => {
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
@@ -13,9 +17,15 @@ const ProfileForm = () => {
     const enteredNewPassword = newPasswordInputRef.current.value;
 
     // add validation
+    const passwordIsValid = checkPassword(enteredNewPassword);
+
+    if (!passwordIsValid) {
+      alert("Password doesn't meet the requirements. Please try again.");
+      return;
+    }
 
     fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBZhsabDexE9BhcJbGxnZ4DiRlrCN9xe24",
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCyA3jsUjk0y6NxwqCK7sZrWYl_3alnsiA",
       {
         method: "POST",
         body: JSON.stringify({
@@ -27,9 +37,24 @@ const ProfileForm = () => {
           "Content-Type": "application/json",
         },
       }
-    ).then((res) => {
-      // assumption: Always succeeds!
-    });
+    )
+      .then((res) => {
+        // assumption: Always succeeds!
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Password changed failed. Please try again.";
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        alert("Password change was successful.");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   };
 
   return (
